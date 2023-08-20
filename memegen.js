d7104b184688896960cc9446415b47dc4e43669b
JaeWon Jin <j1jin286@gmail.com>
2023-08-20 10:37:29 -0700

    requirements completed

--- memegen.js
+++ memegen.js
@@ -5,10 +5,23 @@ const botText = document.querySelector('#bottom-text');
 const previewTop = document.querySelector('#top-preview');
 const previewImg = document.querySelector('.preview-img');
 const previewBot = document.querySelector('#bot-preview');
+const preview = document.querySelector('.preview');
+const result = document.querySelector('.result');
 
 submit.addEventListener('click', function(e){
   e.preventDefault();
-  previewTop.innerText = topText.value;
+  const generate = preview.cloneNode(true);
+  generate.classList.add("meme");
+  const trash = document.createElement('i');
+  trash.className = "fa-regular fa-trash-can";
+  generate.appendChild(trash);
+  result.prepend(generate);
+  topText.value = "";
+  imgSrc.value = "";
+  botText.value = "";
+  previewTop.innerText = "TOP TEXT";
+  previewBot.innerText = "BOT TEXT";
+  previewImg.setAttribute("src", "https://www.peacemakersnetwork.org/wp-content/uploads/2019/09/placeholder.jpg");
 })
 
 topText.addEventListener('input', function(e){
@@ -21,4 +34,10 @@ botText.addEventListener('input', function(e){
 
 imgSrc.addEventListener('input', function(e){
   previewImg.setAttribute("src", e.target.value);
+})
+
+result.addEventListener('click', function(e){
+  if(e.target.className === 'fa-regular fa-trash-can'){
+    e.target.parentElement.remove();
+  }
 })
